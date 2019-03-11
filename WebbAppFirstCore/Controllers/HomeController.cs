96833a8b484c267583fbd8caaceb2dbb1ef6b906
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-03-11 16:17:33 +0100

    Add project files.

---
+++ WebbAppFirstCore/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebbAppFirstCore.Modells;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebbAppFirstCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Core()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Swing()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Swing(String angle, string velocity)
+        {
+            double numAngle = 0;//gör en dubbel med namn numAnglesom jag ger startvärde 0
+            double numVelocity = 0;//samma som ovsn fast num Velocity
+            angle = angle.Replace('.',',');
+            velocity = velocity.Replace('.', ',');
+            Double.TryParse(angle, out numAngle);
+            Double.TryParse(velocity, out numVelocity);
+
+            /*double numAngle = Convert.ToDouble(angle);
+            double numVelocity = Convert.ToDouble(velocity);*/
+           ViewBag.Distance = Golf.Distance(numAngle, numVelocity);
+
+            return View();
+        }
+    }
+}
---
+++ WebbAppFirstCore/Modells/Golf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbAppFirstCore.Modells
+{
+    /*Algorithms
+     * Angle Radians :(Math.PI/180)*angle
+     Distance: mmath.Pow(velocity,2)/GRAVITY*Math.sin(2*angleRadians)
+     Gravity is Equal to 9.8
+     Ex: at 45 and 56 m/s the ball 320m*/
+    public class Golf
+    {
+        static readonly double GRAVITY = 9.8;
+
+        private static double AngleInRadians(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+        public static double Distance(Double Velocity, Double angle)
+        {
+            return Math.Pow(Velocity, 2) / GRAVITY * Math.Sin(2 * AngleInRadians(angle));
+        }
+    }
+}
